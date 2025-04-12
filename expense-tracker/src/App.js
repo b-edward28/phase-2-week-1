1a2bf3162f7218c71c561b713b611f3f935f3484
Brendah Edward <edwardbrendah28@gmail.com>
2025-04-12 16:33:20 +0300

    Adds Expense table

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import ExpenseTable from './components/ExpenseTable';
 
 function App() {
+  const [expenses, setExpenses] = useState([
+    {
+      name: 'Groceries',
+      description: 'Weekly grocery shopping',
+      category: 'Food',
+      amount: 150,
+      date: '2023-10-01',
+    },
+    {
+      name: 'Utilities',
+      description: 'Monthly electricity bill',
+      category: 'Bills',
+      amount: 100,
+      date: '2023-10-05',
+    },
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Expense Tracker</h1>
+      <h4>Start taking control of your finances and life. Record, categorize and analyze your spending</h4>
+      <ExpenseTable expenses={expenses} />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ expense-tracker/src/components/ExpenseTable.jsx
@@ -0,0 +1,27 @@
+function ExpenseTable({ expenses }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Description</th>
+          <th>Category</th>
+          <th>Amount</th>
+          <th>Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        {expenses.map((expense, index) => (
+          <tr key={index}>
+            <td>{expense.name}</td>
+            <td>{expense.description}</td>
+            <td>{expense.category}</td>
+            <td>{expense.amount}</td>
+            <td>{expense.date}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+export default ExpenseTable;
