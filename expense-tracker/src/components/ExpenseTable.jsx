7801f37d49dec9c71ee683e2d01a08474454d32a
Brendah Edward <edwardbrendah28@gmail.com>
2025-04-13 12:15:39 +0300

    Adds expense table

---
+++ expense-tracker/src/components/ExpenseTable.jsx
@@ -0,0 +1,29 @@
+import ExpenseRow from './ExpenseRow.jsx';
+function ExpenseTable({ expenses }) {
+
+    return (
+        <div className="table-responsive">
+            <table className="table table-bordered table-striped">
+                <thead className="table-dark">
+                    <tr>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Category</th>
+                        <th>Amount</th>
+                        <th>Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {expenses.map((expense) => (
+                        <ExpenseRow 
+                        key={expense.id} 
+                        expense={expense}  
+                        />
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default ExpenseTable;
